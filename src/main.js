6644a4d3338dcb73dd9920d9d5ecdc54f7d7daa9
Dominika <dominika.kusarek@gmail.com>
2020-04-11 15:51:09 +0200

    bootstrap and fontawesome

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSun, faCloud, faCloudSun, faCloudSunRain, faCloudShowersHeavy, faCloudRain, faSnowflake } 
+  from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faSun,faCloud,faCloudSun, faCloudSunRain, faCloudShowersHeavy,faCloudRain, faSnowflake)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 
 Vue.config.productionTip = false
 
