09702c1234c5a152ebc76b2d0bca75110feae4db
5kumorek <radoslaw.kumorekit@gmail.com>
2020-04-16 22:25:12 +0200

    push components

---
+++ src/components/CurrentWeather.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="col-sm-4">
+      <h2>Current Weather</h2>
+      <h5 id="weather"></h5>
+      <h5>WeatherVue: {{weatherInfo}}</h5>
+      <div class="weather_img">weather_img</div>
+      <h3>Parameters</h3>
+      <p>NULL</p>
+      <ul class="nav nav-pills flex-column">
+        <li>
+            <p id="temperature"></p>
+            <p>TemperatureVue: {{ temperature }}</p>
+        </li>
+         <li>
+            <p>SomeParam_1:</p>
+        </li>
+        <li>
+            <p>SomeParam_2:</p>
+        </li>
+        <li>
+            <p>SomeParam_3:</p>
+        </li>
+        <li>
+            <p>SomeParam_4:</p>
+        </li>
+        <!-- <li>
+            <p>SomeParam_5:</p>
+        </li> -->
+      </ul>
+      <hr class="d-sm-none">
+    </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'temperature',
+    'weatherInfo'
+  ]
+}
+</script>
+
---
+++ src/components/ExtendedWeather.vue
@@ -0,0 +1,9 @@
+<template>
+    <!-- todo -->
+    <div class="col-sm-4" style="border:1px solid black;">
+      <h2>Forecast Wether</h2>
+      <h5>Here is place for forecast weather</h5>
+      <p>Temporary border</p>
+    </div>
+    <!-- end of todo -->
+</template>
