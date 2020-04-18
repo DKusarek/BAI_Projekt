d9278352a63ca6a3f9df1c76fcec7f2b8ee4d488
Bartek <Bartlomiej.Ropek@sabre.com>
2020-04-18 11:36:44 +0200

    Modified data, added units closes #6

--- src/components/ExtendedWeather.vue
+++ src/components/ExtendedWeather.vue
@@ -2,12 +2,11 @@
     <!-- todo -->
     <div class="col-sm-6" style="border:1px solid black;">
       <h2>Extended Wether</h2>
-      <h5>Wind speed: {{windSpeed}}</h5>
-      <h5>Feels like: {{feelsLike}}</h5>
-      <h5>Minimal temperature: {{tempMin}}</h5>
-      <h5>Maximal temperature: {{tempMax}}</h5>
-      <h5>Humidity: {{humidity}}</h5>
-      <h5>Pressure: {{pressure}}</h5>
+      <h5>Feels like: {{convertFeelsLikeToCelsius}} °C</h5>
+      <h5>Pressure: {{pressure}} hPa</h5>
+      <h5>Humidity: {{humidity}} %</h5>
+      <h5>Clouds: {{clouds}} %</h5>
+      <h5>Rain: {{rain}} %</h5>
     </div>
     <!-- end of todo -->
 </template>
@@ -19,12 +18,11 @@ export default {
     name: 'ExtendedWeather',
     data() {
         return {
-            windSpeed: null,
-            feelsLike: null,
-            tempMin: null,
-            tempMax: null,
-            humidity: null,
-            pressure: null,
+            feelsLike: 0,
+            clouds: 0,
+            humidity: 0,
+            pressure: 0,
+            rain: 0,
             errored: null,
             loading: null
         }
@@ -33,18 +31,22 @@ export default {
         axios
         .get(`${process.env.VUE_APP_URL}/weather?q=London&appid=${process.env.VUE_APP_API_KEY}`)
         .then(response => {
-            this.windSpeed = response['data']['wind']['speed'],
             this.feelsLike = response['data']['main']['feels_like'],
-            this.tempMin = response['data']['main']['temp_min'],
-            this.tempMax = response['data']['main']['temp_max'],
+            this.clouds = response['data']['clouds']['all'],
             this.pressure = response['data']['main']['pressure'],
-            this.humidity = response['data']['main']['humidity']
+            this.humidity = response['data']['main']['humidity'],
+            this.humidity = response['data']['rain']['3h']
           })
         .catch(error => {
             console.log(error)
             this.errored = true
         })
         .finally(() => this.loading = false)
+    },
+    computed: {
+      convertFeelsLikeToCelsius: function () {
+        return Math.round(this.feelsLike - 273.15);
+      }
     }
 }
 </script>
