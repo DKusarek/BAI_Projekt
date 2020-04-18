512ab8e8c024b6e98eea617f3393594c6701293c
Dominika <dominika.kusarek@gmail.com>
2020-04-18 15:03:47 +0200

    fixing bugs

--- src/components/ForecastWeather.vue
+++ src/components/ForecastWeather.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="col-sm-12" style="border:1px solid black;">
+  <div class="col-sm-12" style="border:1px solid black;" v-if="!loading">
     <h2>Forecast Wether</h2>
     <div class="day-forecast" v-for="weather in forecast" :key="weather.id">
       <div class="row">
@@ -34,15 +34,15 @@ export default {
           wind: {
             speed: "",
             direction: ""
-          },
-          errored: null,
-          loading: null,
-          city: "",
-          latitude: 0,
-          longitude: 0,
-          url: null
+          }
         }
-      ]
+      ],
+      errored: null,
+      loading: true,
+      city: "",
+      latitude: 0,
+      longitude: 0,
+      url: null
     };
   },
   created() {
@@ -50,18 +50,19 @@ export default {
     this.latitude = JSON.parse(window.localStorage.getItem("latitude"));
     this.longitude = JSON.parse(window.localStorage.getItem("longitude"));
     if (this.city) {
-      this.url = `${process.env.VUE_APP_URL}/weather?q=${this.city}&appid=${process.env.VUE_APP_API_KEY}`;
+      this.url = `${process.env.VUE_APP_URL}/forecast?q=${this.city}&appid=${process.env.VUE_APP_API_KEY}`;
     } else if (this.latitude && this.longitude) {
-      this.url = `${process.env.VUE_APP_URL}/weather?lat=${this.latitude}&lon=${this.longitude}&appid=${process.env.VUE_APP_API_KEY}`;
+      this.url = `${process.env.VUE_APP_URL}/forecast?lat=${this.latitude}&lon=${this.longitude}&appid=${process.env.VUE_APP_API_KEY}`;
     } else {
-      this.url = `${process.env.VUE_APP_URL}/weather?q=London&appid=${process.env.VUE_APP_API_KEY}`;
+      this.url = `${process.env.VUE_APP_URL}/forecast?q=London&appid=${process.env.VUE_APP_API_KEY}`;
     }
   },
   mounted: function() {
-    axios.get(this.url).then(response => {
-      let forecast = [];
-      response.data.list
-        .forEach((x, index) => {
+    axios
+      .get(this.url)
+      .then(response => {
+        let forecast = [];
+        response.data.list.forEach((x, index) => {
           if (x.dt_txt.indexOf("12:00:00") !== -1) {
             let weather = {
               id: index,
@@ -78,15 +79,15 @@ export default {
             };
             forecast.push(weather);
           }
-        })
-        .catch(error => {
-          console.log(error);
-          this.errored = true;
-        })
-        .finally(() => (this.loading = false));
+        });
 
-      this.forecast = forecast;
-    });
+        this.forecast = forecast;
+      })
+      .catch(error => {
+        console.log(error);
+        this.errored = true;
+      })
+      .finally(() => (this.loading = false));
   }
 };
 </script>
