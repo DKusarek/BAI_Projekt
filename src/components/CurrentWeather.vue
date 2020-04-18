ecaa190c469deb5fc1acbd6293402f3fdf4fdc05
5kumorek <radoslaw.kumorekit@gmail.com>
2020-04-18 23:41:37 +0200

    current weather the most imporstants attributes

--- src/components/CurrentWeather.vue
+++ src/components/CurrentWeather.vue
@@ -1,42 +1,45 @@
 <template>
-    <div class="col-sm-6">
-      <h2>Current Weather</h2>
-      <h5 id="weather"></h5>
-      <h5>WeatherVue: {{weatherInfo}}</h5>
-      <div class="weather_img">weather_img</div>
-      <h3>Parameters</h3>
-      <p>NULL</p>
-      <ul class="nav nav-pills flex-column">
-        <li>
-            <p id="temperature"></p>
-            <p>TemperatureVue: {{ temperature }}</p>
-        </li>
-         <li>
-            <p>SomeParam_1:</p>
-        </li>
-        <li>
-            <p>SomeParam_2:</p>
-        </li>
-        <li>
-            <p>SomeParam_3:</p>
-        </li>
-        <li>
-            <p>SomeParam_4:</p>
-        </li>
-        <!-- <li>
-            <p>SomeParam_5:</p>
-        </li> -->
-      </ul>
-      <hr class="d-sm-none">
+    <div class="col-sm-6" style="border:1px solid black;">
+      <h2>Weather for today</h2>
+      <h6>{{weatherInfo}}</h6>
+      <img v-bind:src="iconSource"/> 
+      <h6>Temperature: {{ temperature }}</h6>
     </div>
 </template>
 
 <script>
 export default {
-  props: [
-    'temperature',
-    'weatherInfo'
-  ]
+  data() {
+    return {
+      temperature: 0,
+      weatherInfo: "",
+      iconId: ""
+    };
+  },
+  computed:{
+      iconSource() {
+        return "http://openweathermap.org/img/wn/"+ this.iconId+"@2x.png";
+    }
+  },
+  mounted: function() {
+    fetch(
+      `${process.env.VUE_APP_URL}/weather?q=London&appid=${process.env.VUE_APP_API_KEY}`,
+      {
+        method: "get"
+      }
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(jsonObj => {
+        this.weatherInfo = jsonObj.weather[0].description;
+        this.temperature =
+          Math.round(
+            (parseFloat(jsonObj.main.temp) - 272.1 + Number.EPSILON) * 100
+          ) / 100;
+        this.iconId = jsonObj.weather[0].icon;
+      });
+  }
 }
 </script>
 
