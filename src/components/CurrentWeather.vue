6c3d4cfda291fcecd0f453bfc926c0b02027278a
5kumorek <radoslaw.kumorekit@gmail.com>
2020-04-19 11:34:53 +0200

    currnet weather with font-awesome and mixin

--- src/components/CurrentWeather.vue
+++ src/components/CurrentWeather.vue
@@ -1,45 +1,79 @@
 <template>
     <div class="col-sm-6" style="border:1px solid black;">
       <h2>Weather for today</h2>
-      <h6>{{weatherInfo}}</h6>
-      <img v-bind:src="iconSource"/> 
-      <h6>Temperature: {{ temperature }}</h6>
+      <h6>{{weather.description}}</h6>
+      <div class="weather-icon" v-if="weather.icon">
+        <font-awesome-icon :icon="weather.icon" size="4x" />
+      </div>
+      <h6>Temperature: {{ weather.temperature }}</h6>
     </div>
 </template>
 
 <script>
+import axios from "axios";
+import { icons } from "./helpers/IconHelper";
+import { conversion } from "./helpers/ConversionHelper";
+
 export default {
+  name: "current-weather",
+  mixins: [icons, conversion],
   data() {
-    return {
-      temperature: 0,
-      weatherInfo: "",
-      iconId: ""
+   return {
+      weather: [
+        {
+          icon: "",
+          temperature: 0,
+          description: "",
+          wind: {
+            speed: "",
+            direction: ""
+          }
+        }
+      ],
+      errored: null,
+      loading: true,
+      city: "",
+      latitude: 0,
+      longitude: 0,
+      url: null
     };
   },
-  computed:{
-      iconSource() {
-        return "http://openweathermap.org/img/wn/"+ this.iconId+"@2x.png";
+  created() {
+    this.city = JSON.parse(window.localStorage.getItem("city"));
+    this.latitude = JSON.parse(window.localStorage.getItem("latitude"));
+    this.longitude = JSON.parse(window.localStorage.getItem("longitude"));
+    if (this.city) {
+      this.url = `${process.env.VUE_APP_URL}/weather?q=${this.city}&appid=${process.env.VUE_APP_API_KEY}`;
+    } else if (this.latitude && this.longitude) {
+      this.url = `${process.env.VUE_APP_URL}/weather?lat=${this.latitude}&lon=${this.longitude}&appid=${process.env.VUE_APP_API_KEY}`;
+    } else {
+      this.url = `${process.env.VUE_APP_URL}/weather?q=London&appid=${process.env.VUE_APP_API_KEY}`;
     }
   },
   mounted: function() {
-    fetch(
-      `${process.env.VUE_APP_URL}/weather?q=London&appid=${process.env.VUE_APP_API_KEY}`,
-      {
-        method: "get"
-      }
-    )
+    axios
+      .get(this.url)
       .then(response => {
-        return response.json();
-      })
-      .then(jsonObj => {
-        this.weatherInfo = jsonObj.weather[0].description;
-        this.temperature =
-          Math.round(
-            (parseFloat(jsonObj.main.temp) - 272.1 + Number.EPSILON) * 100
-          ) / 100;
-        this.iconId = jsonObj.weather[0].icon;
-      });
+            this.weather = {
+              description: response.data.weather[0].description,
+              icon: this.getWeatherIcon(
+                response.data.weather[0].main,
+                response.data.weather[0].description
+              ),
+              temperature: response.data.main.temp,
+              wind: {
+                speed: response.data.wind.speed,
+                direction: this.getWindDirection(response.data.wind.deg)
+              }
+            };
+           
+        })
+        .catch(error => {
+          console.log(error);
+          this.errored = true;
+        })
+        .finally(() => (this.loading = false));
   }
-}
+};
 </script>
 
